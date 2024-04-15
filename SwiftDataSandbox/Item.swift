46aba04f1f7ab1e1044fa94ded64f073f98faf01
gurrium <gurrium@hatena.ne.jp>
2024-04-15 15:16:13 +0900

    Initial Commit

---
+++ SwiftDataSandbox/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftDataSandbox
+//
+//  Created by gurrium on 2024/04/15.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SwiftDataSandbox/SwiftDataSandboxApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftDataSandboxApp.swift
+//  SwiftDataSandbox
+//
+//  Created by gurrium on 2024/04/15.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftDataSandboxApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
